d0c1ec413fcac6a87bdd6926668773b7afaf4797
Arhizmei <arhizmei@gmail.com>
2024-09-25 10:12:14 +0300

    create class Recycler view elements

---
+++ app/src/main/java/com/kravchenkovadim/part10_task33_androidpro/RecyclerViewElements.java
@@ -0,0 +1,28 @@
+package com.kravchenkovadim.part10_task33_androidpro;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewElements  {
+
+    int imageResource;
+    String namePizza;
+    String describePizza;
+
+    public RecyclerViewElements(int imageResource, String namePizza, String describePizza) {
+        this.imageResource = imageResource;
+        this.namePizza = namePizza;
+        this.describePizza = describePizza;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public String getNamePizza() {
+        return namePizza;
+    }
+
+    public String getDescribePizza() {
+        return describePizza;
+    }
+}
